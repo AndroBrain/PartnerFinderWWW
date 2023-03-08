f72c8ec81ac55d1f90bf0df53c30a1f088cc080e
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-03-08 20:14:13 +0100

    Setup navigation to Home with HomePreview

---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,5 @@
+import {HomePagePreview} from "./preview/HomePagePreview";
+
+export function HomePage() {
+    return <HomePagePreview/>
+}
---
+++ src/pages/home/preview/HomePagePreview.js
@@ -0,0 +1,11 @@
+export function HomePagePreview() {
+    return <div className="flex-row">
+        <div className="flex-column flex-1">
+            <span>TEST</span>
+        </div>
+
+        <div className="flex-column flex-1">
+            <span>TEST</span>
+        </div>
+    </div>
+}
