6677bc0a60aa1716eee447fed6c212429f4bf3b5
kajamyk <kajamyk17@gmail.com>
2023-05-08 11:32:28 +0200

    Added home page for logged users

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,10 +1,11 @@
 import {useContext} from "react";
 import {authContext} from "../auth/auth";
 import {HomePagePreview} from "./preview/HomePagePreview";
+import {HomePageLogged} from "./logged/HomePageLogged";
 
 export function HomePage() {
     const {authState} = useContext(authContext)
     return <div>
-        {authState.jwt !== null ? <span className="display-large">Hello logged in user</span> : <HomePagePreview/>}
+        {authState.jwt !== null ? <HomePageLogged/> : <HomePagePreview/>}
     </div>
 }
