741fc12bdaf0bd17921abe82b51b936d2c900085
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-05-19 16:29:48 +0200

    Add getting profileInfo from requests

---
+++ src/pages/profile/GetProfileInfoRequest.js
@@ -0,0 +1,35 @@
+import {apiUrl} from "../../App";
+
+export let GetProfileInfoRequest = (jwt, id, setProfile, setError) => {
+    fetch(`${apiUrl}/user/info?userId=${id}`,
+        {
+            "mode": "cors",
+            "method": "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`
+            },
+        }
+    )
+        .then(response => {
+            if (response.status === 200) {
+                response.json().then(json => {
+                    setProfile(json)
+                })
+            } else {
+                try {
+                    response.json().then(json => {
+                        setError(json)
+                        // setError(response.status + " " + json.title)
+                    })
+                } catch {
+                    setError(response.status);
+                }
+            }
+        })
+        .catch(e => {
+            console.log(e.toString());
+            setError(e.toString());
+        });
+}
--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,16 +1,27 @@
 import {useNavigate} from "react-router-dom";
-import {Box, Card, CardMedia, IconButton, Typography} from "@mui/material";
+import {Box, Card, CardMedia, Typography} from "@mui/material";
 import {IconField} from "../../components/IconField";
 import {Toolbar} from "../../components/Toolbar";
+import {useContext, useEffect, useState} from "react";
+import {GetProfileInfoRequest} from "./GetProfileInfoRequest";
+import {authContext} from "../auth/auth";
 
 export function ProfilePage() {
     const navigate = useNavigate()
+    const [profile, setProfile] = useState({})
+    const [error, setError] = useState(null)
+    const {authState} = useContext(authContext)
 
-    console.log(userData)
+    useEffect(() => {
+        GetProfileInfoRequest(authState.jwt, 2, setProfile, setError)
+    }, [])
+
+    console.log(authState.jwt)
+    console.log("profile", profile)
     return <Box sx={{display: "flex", height: "100%", width: "100%", flexDirection: "column"}}>
         <Toolbar>
             <IconField text={"Wiadomości"} icon={"drawable/send_message.svg"} onClick={() => navigate("/chat")}/>
-            <IconField text={"Strona główna"} icon={"drawable/main_page.svg"} onClick={() => navigate("/main")}/>
+            <IconField text={"Strona główna"} icon={"drawable/main_page.svg"} onClick={() => navigate("/")}/>
         </Toolbar>
         <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
             <Card sx={{
@@ -19,6 +30,7 @@ export function ProfilePage() {
                 width: "80%",
                 minWidth: "min-content"
             }}>
+                {error !== null && <span className="error-span auth-error-span">{error}</span>}
                 <CardMedia
                     component="img"
                     height="500"
@@ -36,7 +48,7 @@ export function ProfilePage() {
                         <Box sx={{display: "flex", flexDirection: "row", justifyContent: "center", margin: "1rem"}}>
                             <Typography variant={"h4"} sx={{
                                 padding: "0.5rem"
-                            }}>{userData.name}, {userData.age}</Typography>
+                            }}>{profile.firstName}, {yearDiff(new Date(profile.dateOfBirth), new Date())}</Typography>
                         </Box>
                         <Typography variant="h5" sx={{padding: "0.5rem"}}>{userData.description}</Typography>
                     </Box>
@@ -46,10 +58,13 @@ export function ProfilePage() {
     </Box>
 }
 
+function yearDiff(dt1, dt2) {
+    var diffYear = (dt2.getTime() - dt1.getTime()) / 1000;
+    diffYear /= (60 * 60 * 24);
+    return Math.abs(Math.round(diffYear / 365.25));
+}
 
 const userData = {
-    name: "Martyna",
     picture: "drawable/martyna.png",
-    age: "21",
     description: "hejka! Nazywam się Martyna i jestem tutaj, aby poznać nowych ludzi. Zawsze lubię spędzać czas na świeżym powietrzu, biegać lub po prostu spacerować. Lubię też czytać dobre książki i gotować smaczne jedzenie. Szukam kogoś, kto jest szczery, inteligentny i z poczuciem humoru. Jeśli chcesz dowiedzieć się więcej, odezwij się do mnie!"
 }
