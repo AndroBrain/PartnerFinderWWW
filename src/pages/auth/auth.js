0cb950712c8d78a47a5ac1f83aba98a11db34848
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-06-11 12:33:44 +0200

    Add login field validation

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,3 +1,8 @@
 import {createContext} from "react";
 
 export let authContext = createContext()
+
+export function validateEmail(email) {
+    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return !pattern.test(email);
+}
